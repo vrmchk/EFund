fa3f26338ac477f5fff9d330714c5219b91936db
vrmchk <ihorveremchuk8@gmail.com>
2024-01-03 22:03:44 +0200

    feat: add text file helper

---
+++ helpers/TextFileHelper/TextFileHelper/Program.cs
@@ -0,0 +1,17 @@
+﻿var resultFilePath = @"D:\KPI_raw\5\CourseWork\EFund\helpers\TextFileHelper\TextFileHelper\result.txt";
+var directory = @"D:\KPI_raw\5\CourseWork\EFund\backend\EFund\EFund.BLL";
+var acceptedFileExtensions = new[] { ".cs" };
+
+var filesPaths = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories)
+    .Where(path => acceptedFileExtensions.Contains(Path.GetExtension(path)))
+    .ToList();
+
+File.Create(resultFilePath).Close();
+
+foreach(var filePath in filesPaths)
+{
+    var fileContent = File.ReadAllText(filePath);
+    var fileName = Path.GetFileName(filePath);
+    var result = $" {fileName}\n\n{fileContent}\n\n";
+    File.AppendAllText(resultFilePath, result);
+}

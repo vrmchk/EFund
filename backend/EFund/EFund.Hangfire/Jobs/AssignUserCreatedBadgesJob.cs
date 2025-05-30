984e339faf13ec5bbd9375a0386528affb8e447d
Ihor Veremchuk <ihor.veremchuk@livesbk.com>
2025-05-30 15:40:00 +0300

    fix badges assignment

--- backend/EFund/EFund.Hangfire/Jobs/AssignUserCreatedBadgesJob.cs
+++ backend/EFund/EFund.Hangfire/Jobs/AssignUserCreatedBadgesJob.cs
@@ -1,17 +1,20 @@
 using EFund.Common.Enums;
 using EFund.DAL.Entities;
+using EFund.DAL.Repositories.Interfaces;
 using EFund.Hangfire.Abstractions;
 using EFund.Hangfire.JobArgs;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFund.Hangfire.Jobs;
 
 public class AssignUserCreatedBadgesJob(
-    UserManager<User> userManager
-)
+    UserManager<User> userManager,
+    IRepository<Badge> badgeRepository)
     : IJob<AssignUserCreatedBadgesJobArgs>
 {
     private readonly UserManager<User> _userManager = userManager;
+    private readonly IRepository<Badge> _badgeRepository = badgeRepository;
     private readonly BadgeType[] _badgeTypesToAssign = [BadgeType.Freshman, BadgeType.UserSince];
 
     public static string Id => nameof(AssignUserCreatedBadgesJob);
@@ -22,11 +25,9 @@ public class AssignUserCreatedBadgesJob(
         if (user == null)
             return;
 
-        user.Badges = _badgeTypesToAssign.Select(b => new Badge
-        {
-            Type = b
-        }).ToList();
+        var badges = await _badgeRepository.Where(b => _badgeTypesToAssign.Contains(b.Type)).ToListAsync(cancellationToken);
 
+        user.Badges = badges;
         await _userManager.UpdateAsync(user);
     }
 }
